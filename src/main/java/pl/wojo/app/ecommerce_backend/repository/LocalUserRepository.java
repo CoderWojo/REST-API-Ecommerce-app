68065df1fda688aca77af332d7922d0e8cd12cd3
Wojciech <wojciechwazny4444@gmail.com>
2025-02-20 21:55:49 +0100

    Add files via upload

--- src/main/java/pl/wojo/app/ecommerce_backend/repository/LocalUserRepository.java
+++ src/main/java/pl/wojo/app/ecommerce_backend/repository/LocalUserRepository.java
@@ -1,5 +1,7 @@
 package pl.wojo.app.ecommerce_backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ public interface LocalUserRepository extends JpaRepository<LocalUser, Long> {
     boolean existsByEmailIgnoreCase(String email);
 
     boolean existsByUsernameIgnoreCase(String username);
+
+    Optional<LocalUser> findByEmail(String email);
 }
